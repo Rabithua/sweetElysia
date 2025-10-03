4a0fcd823ab7733ffc94bb59c3ccaa3d1a2c9e1d
Rabithua <rabithua@gmail.com>
2025-10-03 21:19:18 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,9 @@ import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 import { geminiRoutes } from "./routes/chat";
 
-const app = new Elysia().use(openapi()).use(geminiRoutes);
+const app = new Elysia()
+  .use(openapi())
+  .use(geminiRoutes)
+  .all("/", "hello,sweet elysia!");
 
 export default app;
